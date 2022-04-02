16a66dd9691459c3e9209a2c90217f8d022aa940
Mark J. Healy <gmarkghealy@gmail.com>
2022-04-02 16:37:44 -0400

    re-routing routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,36 @@
 const router = require("express").Router();
-const notesRoutes = require("./notesRoutes");
+const { createNewNote, deleteById, validateNote } = require("../../lib/notes");
+let { notes } = require("../../data/notes");
+const uniqid = require("uniqid");
 
-router.use(notesRoutes);
+// note that getting all notes does not call notes.js file for further processing
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+// delete note using id parameter calling deleteById in notes.js
+router.delete("/notes/:id", (req, res) => {
+  const result = deleteById(req.params.id, notes);
+  if (result) {
+    notes = result;
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// post new note using with unique ID by calling createNEwNote in notes.js
+router.post("/notes", (req, res) => {
+  // add a unique id to the incoming note in the body of the post request
+  const noteId = uniqid.time();
+  req.body.id = noteId;
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const fs = require("fs");
+const path = require("path");
+
+const { notes } = require("./data/notes");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 // parse POST data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
-
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
